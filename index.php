7215b0a64b302e652059f25aec41210924e0e2c7
justin <smefpw@gmail.com>
2019-11-11 22:06:52 +0000

    Update index.php

--- index.php
+++ index.php
@@ -19,11 +19,10 @@
     <body>
         <?php
             $max = 100;
-            $code = openssl_random_pseudo_bytes;
 
             for ($i = 1; $i < $max; $i++) 
             { 
-                $byte = bin2hex($code(1));
+                $byte = bin2hex(openssl_random_pseudo_bytes(1));
                 print "__asm _emit 0x" . $byte . " \\<br>"; 
             }
         ?>
